e467ee55dcbaaa51a5796a1f0296a0c465e37d10
gancaoweidao <2064455206@qq.com>
2022-05-12 22:20:05 +0800

    基础搭建

---
+++ 2021开发中心/demo11IO流/src/demo1字节流/demo1InputStream.java
@@ -0,0 +1,17 @@
+package demo1字节流;
+
+/**
+ * @Author: yqy
+ * @Date: 2022/05/12/21:05
+ * @Description:字节输入流的抽象类
+ */
+public class demo1InputStream {
+    public static void main(String[] args) {
+        //read():读入一个字节 =》返回值表示读入的数据
+        //read(byte[])：读入多个字节 =》 返回值表示本次读入了多少个字节
+        //close:关闭输入流 =》程序创建
+        //IOException:大部分io流都会抛出该异常
+        System.out.println("你好");
+    }
+
+}
