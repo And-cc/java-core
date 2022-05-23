0485bbe1535916b01c0f06b068ed99f238bbdd05
fanpeng <2205642801@qq.com>
2022-05-23 18:26:39 +0800

    第一次提交

---
+++ 2022凌云鹏鹏/src/tst.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName tst.java
+ * @Description TODO
+ * @createTime 2022年05月23日 18:25:00
+ */
+public class tst {
+    @Test
+    public void test(){
+        System.out.println("woshifanpeng");
+    }
+}
